8d87df9359cdf92237331fdd4eb1a07e89a39e01
DMWebDev <89517604+SuperDMWebDev@users.noreply.github.com>
2022-08-19 13:50:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'hadtnt71@gmail.com',
+    pass: 'phambinh3107duyminh22',
+  },
+});
+var mailOptions = {
+  from: 'hadtnt71@gmail.com',
+  to: 'hadtnt73@gmail.com',
+  subject: 'Sending email using nodejs',
+  text: ' That was easy',
+};
+transporter.sendMail(mailOptions, function (err, info) {
+  if (err) {
+    console.log('err', err);
+  } else {
+    console.log('info', info.message);
+  }
+});
